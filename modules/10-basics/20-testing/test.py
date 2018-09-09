ee6a759619105cde06c1dfda47137db85c0ec107
Kirill Mokevnin <mokevnin@gmail.com>
2018-09-09 10:45:30 +0200

    init first module

---
+++ modules/10-basics/20-testing/test.py
@@ -0,0 +1,10 @@
+from asserts import assert_true, assert_equal
+from unittest.mock import patch
+from io import StringIO
+
+with patch('sys.stdout', new=StringIO()) as fake_output:
+    import index
+    actual = fake_output.getvalue().strip()
+    assert_equal(actual, 'Hello, World!')
+
+print(actual)

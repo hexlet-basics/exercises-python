644a5bb059cae9cc975b8d4aeef2ec6f58a494fe
Kirill Mokevnin <mokevnin@gmail.com>
2018-09-25 22:05:50 +0300

    fix hello world

--- modules/10-basics/10-hello-world/test.py
+++ modules/10-basics/10-hello-world/test.py
@@ -1,6 +1,6 @@
-from asserts import assert_true, assert_equal
 from unittest.mock import patch
 from io import StringIO
+from asserts import assert_true, assert_equal
 
 with patch('sys.stdout', new=StringIO()) as fake_output:
     import index

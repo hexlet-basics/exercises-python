72b1bb7f7c8564ff5c9effb3b3bce595bf6bfb09
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-02-12 20:00:58 +0300

    Add the tesing framework (a kind of)

--- modules/10-basics/40-testing/test.py
+++ modules/10-basics/40-testing/test.py
@@ -1,10 +1,3 @@
-from asserts import assert_true, assert_equal
-from unittest.mock import patch
-from io import StringIO
+from hexlet.test import *
 
-with patch('sys.stdout', new=StringIO()) as fake_output:
-    import index
-    actual = fake_output.getvalue().strip()
-    assert_equal(actual, '9780262531962')
-
-print(actual)
+expect_output('9780262531962')
--- modules/25-strings/10-quotes/test.py
+++ modules/25-strings/10-quotes/test.py
@@ -1,11 +1,3 @@
-from asserts import assert_true, assert_equal
-from unittest.mock import patch
-from io import StringIO
+from hexlet.test import *
 
-with patch("sys.stdout", new=StringIO()) as fake_output:
-    import index
-
-    actual = fake_output.getvalue().strip()
-    assert_equal(actual, "\"Khal Drogo's favorite word is \"athjahakar\"\"")
-
-print(actual)
+expect_output("\"Khal Drogo's favorite word is \"athjahakar\"\"")

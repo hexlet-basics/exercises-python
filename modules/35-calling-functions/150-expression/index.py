38e8de668eac64a15f639d88d213601e44f1ffa6
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-02-18 20:21:42 +0300

    Add the '35-calling-function' module (#13)

---
+++ modules/35-calling-functions/150-expression/index.py
@@ -0,0 +1,6 @@
+from hexlet.code_basics import calculate_distance
+
+# BEGIN
+distance = calculate_distance('Winterfell', 'The Twins') + calculate_distance('The Twins', 'The Eyrie')
+print(distance)
+# END

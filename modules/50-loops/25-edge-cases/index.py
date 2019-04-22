bc12f5ccfdf10815d7be32849dbc097877e3a97a
unwnu <44963793+unwnu@users.noreply.github.com>
2019-04-22 23:22:20 +0300

    example optimization

--- modules/50-loops/25-edge-cases/index.py
+++ modules/50-loops/25-edge-cases/index.py
@@ -1,10 +1,2 @@
 def is_arguments_for_substr_correct(string, index, length):
-    if index < 0:
-        return False
-    elif length < 0:
-        return False
-    elif index > len(string) - 1:
-        return False
-    elif index + length > len(string):
-        return False
-    return True
+    return False if index < 0 or index + length > len(string) else True

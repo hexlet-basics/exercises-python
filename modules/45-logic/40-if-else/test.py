29189c54385e961dd3b4229c9f172a7987ad5b2c
Aleksei Pirogov <astynax@users.noreply.github.com>
2019-11-08 16:12:12 +0300

    improve tests for if-else

--- modules/45-logic/40-if-else/test.py
+++ modules/45-logic/40-if-else/test.py
@@ -5,3 +5,7 @@ with TestEnv() as env:
     assert_equal(normalize_url('yandex.ru'), 'https://yandex.ru')
     assert_equal(normalize_url('http://yandex.ru'), 'https://yandex.ru')
     assert_equal(normalize_url('https://yandex.ru'), 'https://yandex.ru')
+    assert_equal(normalize_url('httpsecurity.com'), 'https://httpsecurity.com')
+    assert_equal(normalize_url(
+        'https://httpbin.org/redirect-to?url=http://google.com'
+    ), 'https://httpbin.org/redirect-to?url=http://google.com')

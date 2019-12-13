5c57f7b2613eb4b174c260e9c29343ec854ab4b9
Aleksei Pirogov <astynax@users.noreply.github.com>
2019-12-13 12:15:31 +0300

    make tests more selective

--- modules/50-loops/25-edge-cases/test.py
+++ modules/50-loops/25-edge-cases/test.py
@@ -4,13 +4,9 @@ with TestEnv() as env:
     is_arguments_for_substr_correct = env.expect_defined(
         'is_arguments_for_substr_correct')
     string = 'Sansa Stark';
-    result1 = is_arguments_for_substr_correct(string, -1, 3)
-    assert_equal(result1, False)
-    result2 = is_arguments_for_substr_correct(string, 4, 100)
-    assert_equal(result2, False)
-    result3 = is_arguments_for_substr_correct(string, 10, 10)
-    assert_equal(result3, False)
-    result4 = is_arguments_for_substr_correct(string, 11, 1)
-    assert_equal(result4, False)
-    result5 = is_arguments_for_substr_correct(string, 3, 3)
-    assert_equal(result5, True)
+    end = len(string) - 1
+    assert_equal(is_arguments_for_substr_correct(string, -1, 0), False)
+    assert_equal(is_arguments_for_substr_correct(string, 0, -1), False)
+    assert_equal(is_arguments_for_substr_correct(string, end + 1, 0), False)
+    assert_equal(is_arguments_for_substr_correct(string, end, 1), False)
+    assert_equal(is_arguments_for_substr_correct(string, 3, 3), True)

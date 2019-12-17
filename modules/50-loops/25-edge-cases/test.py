d09bf2ae4e5f576bee596583e43cef5d839ecc0a
Aleksei Pirogov <astynax@users.noreply.github.com>
2019-12-17 10:27:27 +0300

    fix test

--- modules/50-loops/25-edge-cases/test.py
+++ modules/50-loops/25-edge-cases/test.py
@@ -8,5 +8,5 @@ with TestEnv() as env:
     assert_equal(is_arguments_for_substr_correct(string, -1, 0), False)
     assert_equal(is_arguments_for_substr_correct(string, 0, -1), False)
     assert_equal(is_arguments_for_substr_correct(string, end + 1, 0), False)
-    assert_equal(is_arguments_for_substr_correct(string, end, 1), False)
+    assert_equal(is_arguments_for_substr_correct(string, end, 1), True)
     assert_equal(is_arguments_for_substr_correct(string, 3, 3), True)

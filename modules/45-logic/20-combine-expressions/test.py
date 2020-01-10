aeef95a61fa430ff9ce989879168a9ede203670b
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-01-10 16:17:21 +0300

    fix tests

--- modules/45-logic/20-combine-expressions/test.py
+++ modules/45-logic/20-combine-expressions/test.py
@@ -5,5 +5,5 @@ with TestEnv() as env:
     assert_equal(has_targaryen_reference(''), False)
     assert_equal(has_targaryen_reference('Targari'), False)
     assert_equal(has_targaryen_reference('targaryen'), False)
-    assert_equal(has_targaryen_reference('Targaryenish'), False)
+    assert_equal(has_targaryen_reference('Targaryens'), True)
     assert_equal(has_targaryen_reference('Targaryen'), True)

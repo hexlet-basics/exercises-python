7a0a58f9c40e439459e60292a80650832b0a5df3
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-02-17 19:51:02 +0300

    remove unused import

--- modules/25-strings/20-concatenation/test.py
+++ modules/25-strings/20-concatenation/test.py
@@ -3,8 +3,6 @@ from unittest.mock import patch
 from io import StringIO
 
 with patch("sys.stdout", new=StringIO()) as fake_output:
-    import index
-
     actual = fake_output.getvalue().strip()
     assert_equal(actual, 'Winter came for the House of Frey.')
 

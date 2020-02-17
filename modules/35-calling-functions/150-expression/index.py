4d90c548c20ca448a22f2f8a34c9c0b23705ecfa
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-02-17 19:51:11 +0300

    split too long line

--- modules/35-calling-functions/150-expression/index.py
+++ modules/35-calling-functions/150-expression/index.py
@@ -1,6 +1,9 @@
 from hexlet.code_basics import calculate_distance
 
 # BEGIN
-distance = calculate_distance('Winterfell', 'The Twins') + calculate_distance('The Twins', 'The Eyrie')
+winterfell_to_twins = calculate_distance('Winterfell', 'The Twins')
+twins_to_eyrie = calculate_distance('The Twins', 'The Eyrie')
+distance = winterfell_to_twins + twins_to_eyrie
+
 print(distance)
 # END

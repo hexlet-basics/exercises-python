438ca602a4f8f96b471fefc980cf40ca77fb6e8d
Aleksei Pirogov <astynax@users.noreply.github.com>
2020-03-31 16:57:54 +0300

    improve tests

--- modules/50-loops/10-while/test.py
+++ modules/50-loops/10-while/test.py
@@ -2,5 +2,7 @@ from hexlet.test import TestEnv
 
 with TestEnv() as env:
     print_numbers = env.expect_defined('print_numbers')
+    with env.expect_output('2\n1\nfinished!'):
+        print_numbers(2)
     with env.expect_output('4\n3\n2\n1\nfinished!'):
         print_numbers(4)

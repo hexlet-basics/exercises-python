4ce813a4750df95bef004ae05f92c23fe972991a
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-10-20 13:46:24 +0300

    refactor module 7 (define functions)

--- modules/40-define-functions/100-define/test_code.py
+++ modules/40-define-functions/100-define/test_code.py
@@ -2,13 +2,7 @@ import index
 from hexlet.test import expect_output
 
 
-def test1(capsys):
-    index.print_jaimes_line('hey')
-    expected = 'JAIME: hey'
-    expect_output(capsys, expected)
-
-
-def test2(capsys):
-    index.print_jaimes_line('Farewell, my friend...')
-    expected = 'JAIME: Farewell, my friend...'
+def test(capsys):
+    index.print_motto()
+    expected = 'Winter is coming'
     expect_output(capsys, expected)

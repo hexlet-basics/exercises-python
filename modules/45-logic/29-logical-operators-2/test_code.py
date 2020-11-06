438cc6a71bce55ebcc94c61e8d566742ce4750d5
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-11-06 16:02:49 +0300

    fix(45-logic/29-logical-operators-2): add test

--- modules/45-logic/29-logical-operators-2/test_code.py
+++ modules/45-logic/29-logical-operators-2/test_code.py
@@ -5,4 +5,5 @@ def test1():
     assert index.is_neutral_soldier('yellow', 'black')
     assert not index.is_neutral_soldier('red', 'yellow')
     assert not index.is_neutral_soldier('red', 'red')
+    assert not index.is_neutral_soldier('yellow', 'red')
     assert index.is_neutral_soldier('black', 'black')

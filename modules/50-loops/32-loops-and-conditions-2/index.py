74eaafc35ffdd03a6f04bb73892fd663468cdfab
mokevnin <mokevnin@gmail.com>
2020-12-07 16:23:05 -0500

    update loops

--- modules/50-loops/32-loops-and-conditions-2/index.py
+++
@@ -1,8 +0,0 @@
-def get_even_numbers_up_to(number):
-    counter = 1
-    result = ''
-    while counter <= number:
-        if counter % 2 == 0:
-            result = result + str(counter) + ','
-        counter += 1
-    return result

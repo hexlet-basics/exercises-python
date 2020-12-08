bd4bd3a51457ff21b52adfd929193363b59f0195
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-12-08 18:18:31 +0300

    fix test

--- modules/50-loops/30-syntactic-sugar/test_code.py
+++ modules/50-loops/30-syntactic-sugar/test_code.py
@@ -3,6 +3,6 @@ import index
 
 def test1():
     text = 'If I look back I am lost'
-    assert index.filter_string(text, 'b') == 'If I look back I am lost'
+    assert index.filter_string(text, 'w') == 'If I look back I am lost'
     assert index.filter_string(text, 'I') == 'f  look back  am lost'
     assert index.filter_string('zz zorro', 'z') == ' orro'

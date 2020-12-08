7252cdf44ec298b379f2a3c7faae20eec3d32bb1
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-12-08 17:36:30 +0300

    improve loops

---
+++ modules/50-loops/28-build-string/index.py
@@ -0,0 +1,9 @@
+# BEGIN
+def my_substr(string, length):
+    result_string = ''
+    index = 0
+    while index < length:
+        result_string = result_string + string[index]
+        index = index + 1
+
+    return result_string
---
+++ modules/50-loops/28-build-string/test_code.py
@@ -0,0 +1,7 @@
+import index
+
+
+def test1():
+    assert index.my_substr('got', 3) == 'got'
+    assert index.my_substr('got', 2) == 'go'
+    assert index.my_substr('got', 1) == 'g'

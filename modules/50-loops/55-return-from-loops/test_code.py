ae6aab111afd6723262e7eaffb53f0cc79ad946c
Dmitriy Bulygin <47479409+DemetriusStorm@users.noreply.github.com>
2021-01-31 18:34:55 +0300

    Update test_code.py
    Added new test. In case the student checks while index < len(string) - 1

--- modules/50-loops/55-return-from-loops/test_code.py
+++ modules/50-loops/55-return-from-loops/test_code.py
@@ -6,3 +6,4 @@ def test1(capsys):
     assert not index.does_contain('Renly', 'r')
     assert index.does_contain('Tommy', 'm')
     assert not index.does_contain('Tommy', 'd')
+    assert index.does_contain('Tommy', 'y')

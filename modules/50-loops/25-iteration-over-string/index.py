dbd5bbf0b087d53fd615a7f209ba6906214314c4
Roman Ashikov <rmn.shkv@gmail.com>
2022-03-08 14:19:50 +0700

    (50-loops/25-iteration-over-string) fix function name

--- modules/50-loops/25-iteration-over-string/index.py
+++ modules/50-loops/25-iteration-over-string/index.py
@@ -1,5 +1,5 @@
-def print_name_by_symbol(name):
-    i = len(name) - 1
+def print_reversed_word_by_symbol(word):
+    i = len(word) - 1
     while i >= 0:
-        print(name[i])
+        print(word[i])
         i = i - 1

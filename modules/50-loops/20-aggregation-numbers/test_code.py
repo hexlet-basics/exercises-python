47dc2a83560125f10bd0040703afb94578d1a1f8
Roman Ashikov <rmn.shkv@gmail.com>
2022-03-08 14:12:56 +0700

    (50-loops/20-aggregation-numbers) fix function name

--- modules/50-loops/20-aggregation-numbers/test_code.py
+++ modules/50-loops/20-aggregation-numbers/test_code.py
@@ -2,6 +2,6 @@ import index
 
 
 def test1():
-    assert index.multiply_number_from_range(2, 2) == 2
-    assert index.multiply_number_from_range(1, 3) == 6
-    assert index.multiply_number_from_range(1, 5) == 120
+    assert index.multiply_numbers_from_range(2, 2) == 2
+    assert index.multiply_numbers_from_range(1, 3) == 6
+    assert index.multiply_numbers_from_range(1, 5) == 120

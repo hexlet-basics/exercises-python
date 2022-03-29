1ac5ed3afb0a8b1ca4e8ea6643a39696d3c5075a
Sergey Novozhilov <42992333+sergkot2020@users.noreply.github.com>
2022-03-29 14:07:16 +0300

    fix camelcase args
    converted var names to python style

--- modules/40-define-functions/400-default-parameters/index.py
+++ modules/40-define-functions/400-default-parameters/index.py
@@ -1,3 +1,3 @@
-def get_hidden_card(cardNumber, starsCount=4):
-    visibleDigitsLine = cardNumber[-4:]
-    return f"{'*' * starsCount}{visibleDigitsLine}"
+def get_hidden_card(card_number, stars_count=4):
+    visible_digits_line = card_number[-4:]
+    return f"{'*' * stars_count}{visible_digits_line}"

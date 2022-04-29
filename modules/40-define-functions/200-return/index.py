73b3d620df22f04bbebf643021a295bbaa28060a
Aleksandr Litvinov <ssssank@gmail.com>
2022-04-29 18:20:01 +0300

    update return exercise (#205)
    * update return exercise
    
    * fix typo
    
    * delete datetime import
    
    * fix typo
    
    * fix typo

--- modules/40-define-functions/200-return/index.py
+++ modules/40-define-functions/200-return/index.py
@@ -1,8 +1,4 @@
-from datetime import date
-
-
 # BEGIN
-def get_current_year():
-    current_date = date.today()
-    return int(current_date.isoformat()[0:4])
+def get_cube_of_three():
+    return 3 * 3 * 3
 # END

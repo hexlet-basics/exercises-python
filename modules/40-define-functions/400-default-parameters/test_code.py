4291a7f6401ced6d4e0ab50bb7581204b8605dbf
Sergey Novozhilov <42992333+sergkot2020@users.noreply.github.com>
2022-05-05 08:46:44 +0300

    Update test_code.py
    Test should also check the second argument with the default value too

--- modules/40-define-functions/400-default-parameters/test_code.py
+++ modules/40-define-functions/400-default-parameters/test_code.py
@@ -4,3 +4,4 @@ import index
 def test():
     assert index.get_hidden_card('1234123412341234') == '****1234'
     assert index.get_hidden_card('1234123412344321') == '****4321'
+    assert index.get_hidden_card('1234123412344321', 3) == '***4321'

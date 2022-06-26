7cae383e9559714d1bb107a2588b3a7b61bba564
Ivan Gagarinov <dzencot@gmail.com>
2022-06-26 16:43:04 +0500

    improve solution

--- modules/25-strings/10-quotes/index.py
+++ modules/25-strings/10-quotes/index.py
@@ -1,1 +1,1 @@
-print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+print('"Khal Drogo\'s favorite word is "athjahakar""')
--- modules/25-strings/10-quotes/test_code.py
+++ modules/25-strings/10-quotes/test_code.py
@@ -2,5 +2,5 @@ from hexlet.test import expect_output
 
 
 def test(capsys):
-    expected = "\"Khal Drogo's favorite word is \"athjahakar\"\""
+    expected = '"Khal Drogo\'s favorite word is "athjahakar""'
     expect_output(capsys, expected)

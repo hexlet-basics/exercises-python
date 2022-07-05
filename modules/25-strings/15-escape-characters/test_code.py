ed7059c0c945548bacbac23c592f3f6fd754bc4e
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-07-05 16:04:39 +0400

    refactor solution - remove extra escaping slashes

--- modules/25-strings/15-escape-characters/test_code.py
+++ modules/25-strings/15-escape-characters/test_code.py
@@ -2,6 +2,5 @@ from hexlet.test import expect_output
 
 
 def test(capsys):
-    expected = "- Did Joffrey agree?\n" \
-               "- He did. He also said \"I love using \\n\"."
+    expected = '- Did Joffrey agree?\n- He did. He also said "I love using \\n".'  # noqa: E501
     expect_output(capsys, expected)

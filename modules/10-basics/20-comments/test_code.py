026528127f179f443d1f5829b0a3764fdb75a8f2
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-08-25 17:31:08 +0400

    fix test

--- modules/10-basics/20-comments/test_code.py
+++ modules/10-basics/20-comments/test_code.py
@@ -1,5 +1,5 @@
 def test():
     with open('index.py') as file:
-        comment = file.read()
-        assert comment == '# You know nothing, Jon Snow!\n'
+        comment = file.read().rstrip()
+        assert comment == '# You know nothing, Jon Snow!'
         print(comment)

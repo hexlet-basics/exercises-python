3c4ba36d239e9007ed8badee628ea172cc102d9e
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-08-31 15:27:21 +0400

    fix tests

--- modules/50-loops/55-return-from-loops/test_code.py
+++ modules/50-loops/55-return-from-loops/test_code.py
@@ -2,8 +2,8 @@ from index import is_contains_char
 
 
 def test_is_contains_char():
-    assert is_contains_char('Hexlet', 'H') == True
-    assert is_contains_char('Hexlet', 'h') == False
-    assert is_contains_char('Awesomeness', 'm') == True
-    assert is_contains_char('Awesomeness', 'd') == False
-    assert is_contains_char('Awesomeness', 'o') == True
+    assert is_contains_char('Hexlet', 'H') is True
+    assert is_contains_char('Hexlet', 'h') is False
+    assert is_contains_char('Awesomeness', 'm') is True
+    assert is_contains_char('Awesomeness', 'd') is False
+    assert is_contains_char('Awesomeness', 'o') is True

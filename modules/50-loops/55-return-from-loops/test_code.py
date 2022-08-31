010eb5e531a056868ae4f1741fff205c5f3c0305
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-08-31 12:45:33 +0400

    fix test

--- modules/50-loops/55-return-from-loops/test_code.py
+++ modules/50-loops/55-return-from-loops/test_code.py
@@ -1,9 +1,9 @@
-import index
+from index import is_contains_char
 
 
-def test1(capsys):
-    assert index.is_contains_char('Hexlet', 'H')
-    assert not index.is_contains_char('Hexlet', 'h')
-    assert index.is_contains_char('Awesomeness', 'm')
-    assert not index.is_contains_char('Awesomeness', 'd')
-    assert index.is_contains_char('Awesomeness', 'o')
+def test_is_contains_char():
+    assert is_contains_char('Hexlet', 'H') == True
+    assert is_contains_char('Hexlet', 'h') == False
+    assert is_contains_char('Awesomeness', 'm') == True
+    assert is_contains_char('Awesomeness', 'd') == False
+    assert is_contains_char('Awesomeness', 'o') == True

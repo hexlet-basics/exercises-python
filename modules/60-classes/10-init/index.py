403bcde22fd6da2e76245365a8a56c0376583bbf
Камран <kem@192.168.1.13>
2023-05-16 19:38:31 +0300

    add classes

---
+++ modules/60-classes/10-init/index.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p = Person("John", 20)
+print(p.name, p.age)
---
+++ modules/60-classes/10-init/test_code.py
@@ -0,0 +1,7 @@
+import index
+
+
+def test_class():
+    person = index.Person('John', 20)
+    assert person.name == 'John'
+    assert person.age == '20'

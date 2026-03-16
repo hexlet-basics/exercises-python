22a8589afc235e79c52c298a28f4824a0c2f9eae
Nikolay Gagarinov <feycot@gmail.com>
2026-03-16 18:21:28 +0500

    update

--- modules/50-loops/30-syntactic-sugar/index.py
+++ modules/50-loops/30-syntactic-sugar/index.py
@@ -1,9 +1,11 @@
-def filter_string(text: str, char: str) -> str:
+def filter_string(text, char):
     index = 0
     result = ""
+
     while index < len(text):
         current_char = text[index]
         if current_char != char:
-            result = f"{result}{current_char}"
+            result += current_char
         index += 1
+
     return result
